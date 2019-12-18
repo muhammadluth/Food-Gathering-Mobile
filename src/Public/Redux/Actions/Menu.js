8b2f1a7f9fb6c3bb141aeaaf39a0d79e3456552f
Muhammad Luthfi <muhammadluthfi059@gmail.com>
2019-12-18 09:32:31 +0700

    'latest'

--- src/Public/Redux/Actions/Menu.js
+++ src/Public/Redux/Actions/Menu.js
@@ -1,5 +1,4 @@
 import Http from '../../Utils/Http';
-import Axios from 'axios';
 
 export const getMenu = test => {
   console.log('action ' + test);
@@ -7,9 +6,7 @@ export const getMenu = test => {
     type: 'GET_MENU',
     payload: new Promise((resolve, reject) => {
       const {search = '', sort = '', type = ''} = test;
-      Axios.get(
-        `https://point-of-sales-restfull-api-v1.herokuapp.com/api/v1/product?sort=${sort}&type=${type}&search=${search}`,
-      )
+      Http.get(`/api/v1/product?sort=${sort}&type=${type}&search=${search}`)
         .then(result => resolve(result))
         .catch(error => reject(error));
     }),
